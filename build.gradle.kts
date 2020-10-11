621842bf780a75dea42cb1aa9ea77c366362e0eb
tyler-omgcobra <tyler@omgcobra.org>
2020-10-10 21:52:03 -0400

    Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
+  id("com.jfrog.bintray") version "1.8.0"
+  id("maven-publish")
   kotlin("js") version "1.4.10"
 }
 group = "org.omgcobra"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 kotlin {
   js {
@@ -24,6 +26,13 @@ kotlin {
     useCommonJs()
   }
 }
+repositories {
+  mavenLocal()
+  mavenCentral()
+  jcenter()
+  maven("https://kotlin.bintray.com/kotlinx")
+  maven("https://dl.bintray.com/kotlin/kotlin-js-wrappers")
+}
 dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.0")
   implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.2")
@@ -31,4 +40,22 @@ dependencies {
   implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.123-kotlin-1.4.10")
   implementation("org.jetbrains:kotlin-react-router-dom:5.1.2-pre.123-kotlin-1.4.10")
   implementation("org.jetbrains:kotlin-styled:5.2.0-pre.123-kotlin-1.4.10")
+}
+publishing {
+  publications {
+    create<MavenPublication>("default") {
+      from(components["kotlin"])
+      artifact(tasks.getByName("kotlinSourcesJar"))
+    }
+  }
+  repositories {
+    mavenLocal()
+  }
+}
+
+bintray {
+  user = System.getProperty("bintray.user")
+  key = System.getProperty("bintray.key")
+  publish = true
+  setPublications("default")
 }
